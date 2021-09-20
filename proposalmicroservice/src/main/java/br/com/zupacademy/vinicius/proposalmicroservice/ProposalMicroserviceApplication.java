02b145aed32122ab2ae31b114bca2d01d3678ee4
viniciusferreira <vinicius.ricardo@zup.com.br>
2021-09-20 08:07:28 -0300

    Fase 4 - Iniciando o projeto Microsserviço de Propostas

---
+++ proposalmicroservice/src/main/java/br/com/zupacademy/vinicius/proposalmicroservice/ProposalMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.vinicius.proposalmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposalMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProposalMicroserviceApplication.class, args);
+	}
+
+}
